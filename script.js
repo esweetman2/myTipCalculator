74b74a354bc3b09ec5335e063db8f75840e0d928
eric sweetman <esweetman2@gmail.com>
2019-07-26 11:29:36 -0400

    updated JS

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
 
 //tip calculator
 function calTip(){
-    const bill = Number(document.getElementById("bill").value);
+    let bill = document.getElementById("bill");
     const percent20 = Number(document.getElementById("percent20").innerHTML);
     const percent15 = Number(document.getElementById("percent15").innerHTML);
     const percent10 = Number(document.getElementById("percent10").innerHTML);
+
     
-    
-    
-    function display1(){
-      if(bill === "" || bill <= 0){
-        return alert("Enter a vaild amount please!");
-      }else if(isNaN(bill)){
-        return alert("Enter a vaild amount please!");
-      }
-     }
-     setTimeout(display1, 750);
-    
-    setTimeout(display, 750);
-      function display(){
-        if(isNaN(bill)){
-          document.getElementById("amount20").innerHTML = "$ " + "0.00";
-          document.getElementById("amount15").innerHTML = "$ " + "0.00";
-          document.getElementById("amount10").innerHTML = "$ " + "0.00";
-        }else{
-          let tip20 = (bill * percent20) /100;
-            document.getElementById("amount20").innerHTML = "$ " + tip20.toFixed(2);
-          let tip15 = (bill * percent15) /100;
-            document.getElementById("amount15").innerHTML = "$ " + tip15.toFixed(2);
-          let tip10 = (bill * percent10) /100;
-            document.getElementById("amount10").innerHTML ="$ " + tip10.toFixed(2);
-        }
-     }
+  setTimeout(display, 750);
+  function display(){
+    if(bill.value === "" || bill.value <= 0 || isNaN(bill.value)){
+      return alert("Enter a vaild amount please!");
+    }else{
+      let tip20 = (bill.value * percent20) /100;
+        document.getElementById("amount20").innerHTML = "$ " + tip20.toFixed(2);
+      let tip15 = (bill.value * percent15) /100;
+        document.getElementById("amount15").innerHTML = "$ " + tip15.toFixed(2);
+      let tip10 = (bill.value * percent10) /100;
+        document.getElementById("amount10").innerHTML ="$ " + tip10.toFixed(2);
+        bill.value = '';
+    }
   }
+  count();
+  progressBar();
+}
    
   
   function count(){
@@ -50,8 +41,7 @@ function calTip(){
   }
   
 
-  function progressBar(){
-    
+  function progressBar(){ 
     let elem = document.getElementById("fill"); 
     let width = 1;
     let id = setInterval(frame, 5);
@@ -66,6 +56,4 @@ function calTip(){
   }
   
   const calc = document.getElementById("btn");
-  calc.addEventListener("click", function (){calTip();});
-  calc.addEventListener("click", function (){progressBar();});
-  calc.addEventListener("click", function (){count();});
+  calc.addEventListener("click", calTip);
